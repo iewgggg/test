330b6ae92933126a7d91fdcc6d2f68cc82d77a50
xxx <xxx@email>
2020-02-14 21:09:35 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,42 @@
+import { IConfig } from 'umi-types';
+
+const { ENV_USER } = process.env;
+
+// ref: https://umijs.org/config/
+const config: IConfig = {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        { path: '/', component: '../pages/index' },
+      ],
+    },
+  ],
+  context: {
+    ENV_USER,
+  },
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'umi',
+      dll: true,
+
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+};
+
+export default config;
